75556e86d5c146e5b5fe86fe091efa1c7703bbd7
Darien Bernal <darien.bernal@gmail.com>
2025-05-06 17:57:34 -0400

    Completed Lab 2.1.5

---
+++ Lab-2.1.5/index.py
@@ -0,0 +1,15 @@
+# Use the print() function to print the line Hello, Python! to the screen. Use double quotes around the string.
+print("Hello, Python!")
+
+# Having done that, use the print() function again, but this time print your first name.
+print("Darien")
+
+# Remove the double quotes and run your code. Watch Python's reaction. What kind of error is thrown?
+# print(Darien) 
+# Python shows and eError, saying name 'Darien is not defined. Points to where the error is
+
+# Then, remove the parentheses, put back the double quotes, and run your code again. What kind of error is thrown this time?
+# print"Darien"
+# Python tells me what I'm missing to call the print function
+
+# Experiment as much as you can. Change double quotes to single quotes, use multiple print() functions on the same line, and then on different lines. See what happens.
