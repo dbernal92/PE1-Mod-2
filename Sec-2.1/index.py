2092fe3efd95b85ad30e78cfde1b9fc1de8f28dc
Darien Bernal <darien.bernal@gmail.com>
2025-05-06 18:06:36 -0400

    Create newline with print function

---
+++ Sec-2.1/index.py
@@ -0,0 +1,6 @@
+print("Hello, world!")
+
+# 2.1.7 - Instructions
+print("The itsy bitsy spider climbed up the waterspout.")
+print() #Creates a newline in the output console
+print("Down came the rain and washed the spider out.")
