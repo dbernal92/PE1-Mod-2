0bec66c3323a6bd196355d5032d113fbe73f996d
Darien Bernal <darien.bernal@gmail.com>
2025-05-13 17:31:59 -0400

    Python escape and newline examples

--- Sec-2.1/index.py
+++ Sec-2.1/index.py
@@ -4,3 +4,12 @@ print("Hello, world!")
 print("The itsy bitsy spider climbed up the waterspout.")
 print() #Creates a newline in the output console
 print("Down came the rain and washed the spider out.")
+
+# 2.1.8 - Python escape and newline characters
+print("The itsy bitsy spider\nclimbed up the waterspout.")
+print()
+print("Down came the rain\nand washed the spider out.")
+
+# Escape character
+# print("\") - Creates an error, has to be doubled to print the \
+# print("\\")
